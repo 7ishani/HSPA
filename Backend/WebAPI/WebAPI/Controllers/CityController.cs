754a96eddf13db17e1ec50135841749baa1a2a06
Ishani Perera <ishanidilu@gmail.com>
2020-11-27 19:30:25 +0530

    used repository patterns

--- Backend/WebAPI/WebAPI/Controllers/CityController.cs
+++ Backend/WebAPI/WebAPI/Controllers/CityController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAPI.Data;
+using WebAPI.Data.Repo;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -13,15 +16,54 @@ namespace WebAPI.Controllers
     public class CityController : ControllerBase
     {
         private readonly DataContext dc;
-        public CityController(DataContext dc)
+        private readonly ICityRepository repo;
+
+        public CityController(DataContext dc, ICityRepository repo)
         {
             this.dc = dc;
+            this.repo = repo;
         }
+
+        //Get  api/city
         [HttpGet]
-        public IActionResult GetCities()
+        public async Task<IActionResult> GetCities()
         {
-            var cities = dc.Cities.ToList();
-            return Ok(cities);
+            var cities = await repo.GetCitiesAsync();
+            return Ok(cities); 
+        }
+
+        //Post api/city/add?cityname=chilaw
+        //[HttpPost("add")]
+
+        //Post api/city/add/Moratuwa
+        //[HttpPost("add/{cityName}")]
+
+        //public async Task<IActionResult> AddCity(string cityName)
+      //  public async Task<IActionResult> AddCity(string cityName)
+       // {
+         //   City city = new City();
+        //    city.name = cityName;
+        //    await dc.Cities.AddAsync(city);
+         //   await dc.SaveChangesAsync();
+         //   return Ok(city);
+       // }
+
+        //Post api/city/post --Post the data in JSON format--
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            repo.AddCity(city);
+            await repo.SaveAsync();
+            return StatusCode(201);
+        }
+
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            repo.DeleteCity(id);
+            await repo.SaveAsync();
+            return Ok(id);
         }
     }
 }
